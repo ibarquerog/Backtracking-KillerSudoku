b0caa5495cb0e8a0baecc32f93de4732b44f358a
Pvillab <paulvillabeita@gmail.com>
2018-10-09 21:38:11 -0600

    SingleBlock class.

--- KillerSudoku/SingleBlock.cs
+++ KillerSudoku/SingleBlock.cs
@@ -8,6 +8,16 @@ using System.Threading.Tasks;
 
     class SingleBlock
     {
+    private int value;
+    private int x;
+    private int y;
+    private String figure;
 
-
+    public SingleBlock(int x, int y)
+    {
+        this.value = 0;
+        this.x = x;
+        this.y = y;
+        this.figure = null;
     }
+}
